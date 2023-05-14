3b3433c2fdddfb4ec47572776495336374113a9e
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2023-05-14 15:30:06 +0100

    Initial input handling in place

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import tcod
 
+from lawrogue.actions import EscapeAction, MovementAction
+from lawrogue.input_handlers import EventHandler
+
 
 def main():
     screen_width = 80
@@ -12,6 +15,8 @@ def main():
     tileset = tcod.tileset.load_tilesheet(
         "resources/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    event_handler = EventHandler()
 
     with tcod.context.new_terminal(
         screen_width,
@@ -25,7 +30,16 @@ def main():
             root_console.print(x=player_x, y=player_y, string="@")
             context.present(root_console)
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
