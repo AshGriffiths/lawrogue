78f3f566c8f63d31d9965d327dd2c7274e78f0af
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2023-05-22 21:00:13 +0100

    Add main menu, refactor main.py

--- main.py
+++ main.py
@@ -1,56 +1,23 @@
 #!/usr/bin/env python
-import copy
 import traceback
 
 import tcod
 
-from lawrogue import color, entity_factories
-from lawrogue.engine import Engine
+from lawrogue import color
 from lawrogue.exceptions import QuitWithoutSaving
 from lawrogue.input_handlers import BaseEventHandler, EventHandler, MainGameEventHandler
-from lawrogue.procgen import generate_dungeon
+from lawrogue.setup_game import MainMenu
 
 
 def main():
     screen_width = 80
     screen_height = 50
 
-    map_width = 80
-    map_height = 43
-
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    max_monsters_per_room = 2
-    max_items_per_room = 2
-
     tileset = tcod.tileset.load_tilesheet(
         "resources/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    player = copy.deepcopy(entity_factories.player)
-
-    engine = Engine(player=player)
-
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
-        engine=engine,
-    )
-
-    engine.update_fov()
-
-    engine.message_log.add_message(
-        "Hello and welcome, adventurer, to LAW rogue!", color.welcome_text
-    )
-
-    handler: BaseEventHandler = MainGameEventHandler(engine)
+    handler: BaseEventHandler = MainMenu()
 
     with tcod.context.new(
         columns=screen_width,
